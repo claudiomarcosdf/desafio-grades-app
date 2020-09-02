b93a47c31acbf816d5ddc88955b1189e6db4065d
claudiomarcosdf <claudio.marcos.df@gmail.com>
2020-09-02 17:43:55 -0300

    Criação variável REACT_APP_BASE_URL

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
-
 //Define a URL base da origem para consumo do servico
+
+console.log('DOTENV: ' + process.env.REACT_APP_BASE_URL);
 export default axios.create({
-  baseURL: process.env.BASE_URL,
+  baseURL: process.env.REACT_APP_BASE_URL,
   headers: {
     'Content-type': 'application/json',
   },
